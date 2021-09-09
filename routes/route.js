0bdd9f360af50e5901002127a240b7e07b3a501f
Claudia Angelina <claudiaangelina@Claudias-Air.domain.name>
2021-09-09 20:47:19 +0700

    crud obat jadi dan bahan baku

---
+++ routes/route.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+const obatJadiController = require('../controller/obatJadiController')
+const bahanBakuController = require('../controller/bahanBakuController')
+
+router.get('/obatjadi', obatJadiController.selectAll)
+router.get('/obatjadi/:id', obatJadiController.selectByParams)
+router.post('/obatjadi/insert', obatJadiController.insert)
+router.post('/obatjadi/update/:id', obatJadiController.update)
+router.post('/obatjadi/delete/:id', obatJadiController.delete)
+
+router.get('/bahanbaku', bahanBakuController.selectAll)
+router.get('/bahanbaku/:id', bahanBakuController.selectByParams)
+router.post('/bahanbaku/insert', bahanBakuController.insert)
+router.post('/bahanbaku/update/:id', bahanBakuController.update)
+router.post('/bahanbaku/delete/:id', bahanBakuController.delete)
+
+module.exports = router;
