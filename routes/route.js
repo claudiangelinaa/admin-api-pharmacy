19074251f44b56da82d2ad3e64a81d6ea8841b2d
Haryo Novianto <haryoharyo2@gmail.com>
2021-09-26 23:13:56 +0700

    sales-report

--- routes/route.js
+++ routes/route.js
@@ -1,22 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const obatJadiController = require('../controller/obatJadiController')
-const bahanBakuController = require('../controller/bahanBakuController')
-const transactionController = require('../controller/transactionController')
+const obatJadiController = require("../controller/obatJadiController");
+const bahanBakuController = require("../controller/bahanBakuController");
+const transactionController = require("../controller/transactionController");
 
-router.get('/obatjadi', obatJadiController.selectAll)
-router.get('/obatjadi/:id', obatJadiController.selectByParams)
-router.post('/obatjadi/insert', obatJadiController.insert)
-router.post('/obatjadi/update/:id', obatJadiController.update)
-router.post('/obatjadi/delete/:id', obatJadiController.delete)
+router.get("/obatjadi", obatJadiController.selectAll);
+router.get("/obatjadi/:id", obatJadiController.selectByParams);
+router.post("/obatjadi/insert", obatJadiController.insert);
+router.post("/obatjadi/update/:id", obatJadiController.update);
+router.post("/obatjadi/delete/:id", obatJadiController.delete);
 
+router.get("/getTransaction", transactionController.selectAllTransaction);
+router.get("/sales-report", transactionController.salesReport);
 
-router.get('/getTransaction', transactionController.selectAllTransaction)
-router.get('/bahanbaku', bahanBakuController.selectAll)
-router.get('/bahanbaku/:id', bahanBakuController.selectByParams)
-router.post('/bahanbaku/insert', bahanBakuController.insert)
-router.post('/bahanbaku/update/:id', bahanBakuController.update)
-router.post('/bahanbaku/delete/:id', bahanBakuController.delete)
+router.get("/bahanbaku", bahanBakuController.selectAll);
+router.get("/bahanbaku/:id", bahanBakuController.selectByParams);
+router.post("/bahanbaku/insert", bahanBakuController.insert);
+router.post("/bahanbaku/update/:id", bahanBakuController.update);
+router.post("/bahanbaku/delete/:id", bahanBakuController.delete);
 
 module.exports = router;
